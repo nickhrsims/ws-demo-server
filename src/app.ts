d0a1611003af4375fbeb273beceb9da6ca9f7f47
Nicholas H.R. Sims <nickhrsims@gmail.com>
2022-07-18 17:06:19 -0600

    feat: initial source

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import websocket from 'express-ws';
+
+const app = express();
+
+websocket(app);
+
+const api = express.Router();
+
+api.ws('/', (socket) => {
+    socket.on('connection', () => {
+        console.log('Connection Established');
+    });
+    socket.on('message', (data) => {
+        console.log(data);
+    });
+    socket.on('close', (code, reason) => {
+        console.log(`WebSocket Session closed with ${code} (${reason})`);
+    });
+});
+
+app.use('/ws', api);
+
+export default app;
